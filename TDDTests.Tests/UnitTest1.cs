c0cf0e7dfa8380bada0648959fc19eac7f8ab4db
karolc222 <103419236+karolc222@users.noreply.github.com>
2026-03-23 16:40:46 +0000

    Test for all direction.right input

--- TDDTests.Tests/UnitTest1.cs
+++ TDDTests.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 
 
+//using System.ComponentModel;
+//using System.Drawing;
 using TDDTests.Core;
 
 namespace TDDTests.Tests
@@ -22,6 +24,34 @@ namespace TDDTests.Tests
             Point result = compass.Rotate(Point.North, Direction.Right);
             //Assert
             Assert.That(result, Is.EqualTo(Point.East));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            //Arrange
+            Compass compass = new Compass(Point.North);
+
+
+            //Act
+
+            Assert.Multiple(() =>
+            {
+                Point resultN = compass.Rotate(Point.North, Direction.Right);
+                Assert.That(resultN, Is.EqualTo(Point.East));
+
+                Point resultE = compass.Rotate(Point.East, Direction.Right);
+                Assert.That(resultE, Is.EqualTo(Point.South));
+
+                Point resultS = compass.Rotate(Point.South, Direction.Right);
+                Assert.That(resultS, Is.EqualTo(Point.West));
+
+                Point resultW = compass.Rotate(Point.West, Direction.Right);
+                Assert.That(resultW, Is.EqualTo(Point.North));
+
+                }
+
+            );
         }
     }
 }
