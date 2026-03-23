a98afe2e706cb6fe2036aa28c09925017357b05a
Yosefgid <jossykasshun@yahoo.com>
2026-03-23 14:45:50 +0000

    Feat: Add a test method

--- TDDTests.Test/CompassTests.cs
+++ TDDTests.Test/CompassTests.cs
@@ -1,25 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+//using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using TDDTests.Core;
 
 namespace TDDTests.Test
 {
     public class CompassTests
     {
-        [Test]
         //Return x when passed y
         //Ensure that when the rotate method is passed a point
         //Facing North and a Right direction it should return Point.East
+
+        [Test]
         public void Rotate_East_When_Rotating_North_Right()
         {
             //Arrange
+            Compass compass = new Compass(Point.North);
 
             //Act
-
+            Point result = compass.Rotate(Point.North, Direction.Right);
             //Assert
-
+            Assert.That(result, Is.EqualTo(Point.East));
+         
         }
     }
 }
