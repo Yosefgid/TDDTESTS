14446b80bc7284439495f5c371fc44ac3056df2a
Yosefgid <jossykasshun@yahoo.com>
2026-03-23 15:03:21 +0000

    Fix: Delete Old test proj

---
+++ TDDTests.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+
+
+using TDDTests.Core;
+
+namespace TDDTests.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            //Arrange
+            Compass compass = new Compass(Point.North);
+
+            //Act
+            Point result = compass.Rotate(Point.North, Direction.Right);
+            //Assert
+            Assert.That(result, Is.EqualTo(Point.East));
+        }
+    }
+}
